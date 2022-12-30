6c79524fe741ce7e3438d183f57912817a77bff4
Alex Geis <siegxela@gmail.com>
2022-12-29 18:28:12 -0700

    add random info section - style header and footer to work with new sections pushing page height past 100vh

--- CSS-Landing-Page-1/src/App.tsx
+++ CSS-Landing-Page-1/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
+import { InfoSection } from "./components/InfoSection";
 import { Footer } from "./components/Footer";
 
 function App() {
@@ -13,42 +14,7 @@ function App() {
 		<>
 			<Header />
 			<Hero />
-			<div className="App">
-				<div>
-					<a
-						href="https://vitejs.dev"
-						target="_blank"
-					>
-						<img
-							src="/vite.svg"
-							className="logo"
-							alt="Vite logo"
-						/>
-					</a>
-					<a
-						href="https://reactjs.org"
-						target="_blank"
-					>
-						<img
-							src={reactLogo}
-							className="logo react"
-							alt="React logo"
-						/>
-					</a>
-				</div>
-				<h1>Vite + React</h1>
-				<div className="card">
-					<button onClick={() => setCount((count) => count + 1)}>
-						count is {count}
-					</button>
-					<p>
-						Edit <code>src/App.tsx</code> and save to test HMR
-					</p>
-				</div>
-				<p className="read-the-docs">
-					Click on the Vite and React logos to learn more
-				</p>
-			</div>
+			<InfoSection />
 			<Footer />
 		</>
 	);
---
+++ CSS-Landing-Page-1/src/components/InfoSection.tsx
@@ -0,0 +1,27 @@
+import "./InfoSection.css";
+
+export const InfoSection = () => {
+	return (
+		<section id="randoInfoSection">
+			<h2>Some random information.</h2>
+			<div className="cardSection">
+				<div className="card">
+					<div className="box"></div>
+					<p>this is some subtext under an illustration or image</p>
+				</div>
+				<div className="card">
+					<div className="box"></div>
+					<p>this is some subtext under an illustration or image</p>
+				</div>
+				<div className="card">
+					<div className="box"></div>
+					<p>this is some subtext under an illustration or image</p>
+				</div>
+				<div className="card">
+					<div className="box"></div>
+					<p>this is some subtext under an illustration or image</p>
+				</div>
+			</div>
+		</section>
+	);
+};
