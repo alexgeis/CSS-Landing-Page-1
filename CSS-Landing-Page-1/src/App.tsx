017e89207eb7002b3a4be9d8f7c764644bd8c5e5
Alex Geis <siegxela@gmail.com>
2022-12-31 09:09:54 -0700

    update hero content with styling - better center elements and more padding around for breathability

--- CSS-Landing-Page-1/src/App.tsx
+++ CSS-Landing-Page-1/src/App.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { InfoSection } from "./components/InfoSection";
